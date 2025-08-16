f1375c644e653039a9fa924451906a92e49a842c
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2025-08-17 00:49:47 +0530

    Added manifest for pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+import withPWA from 'next-pwa';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your existing Next.js configuration
+};
+
+// Define the PWA configuration
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+};
+
+// Export the combined configuration
+export default withPWA(pwaConfig)(nextConfig);
