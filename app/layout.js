01770cd296f2c98a26f11d45fc7f434f607b24d3
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2025-08-17 03:06:53 +0530

    once more try

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
        <head>
         <link rel="manifest" href="./manifest.json" />
+        <link rel="icon" href="/smallico.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
