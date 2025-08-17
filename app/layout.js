b1045b73ef8d637ec45a06399ba2dbd6e48c41c3
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2025-08-17 11:14:24 +0530

    trying for icon in pwa

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
        <head>
         <link rel="manifest" href="./manifest.json" />
-        <link rel="icon" href="/smallico.png" />
+        <link rel="icon" href="./smallico.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
