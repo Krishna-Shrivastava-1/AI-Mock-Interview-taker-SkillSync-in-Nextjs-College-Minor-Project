3b2d01bdbdcf095cc3083b3eadf321a99994170f
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2025-08-29 23:28:34 +0530

    fixed user profile view

--- app/api/auth/getuserbyid/[id]/route.js
+++ app/api/auth/getuserbyid/[id]/route.js
@@ -11,14 +11,14 @@ export async function GET(req, { params }) {
   try {
     const { id } = await params; // Getting user id
     await database();
-const authHeader = req.headers.get('authorization')
-const idHeaderofUser = authHeader?.split(' ')[1]
-if(!authHeader || id !== idHeaderofUser){
-    return NextResponse.json({
-        messaage:'Unauthorized User',
-        success:false,
-    })
-}
+// const authHeader = req.headers.get('authorization')
+// const idHeaderofUser = authHeader?.split(' ')[1]
+// if(!authHeader || id !== idHeaderofUser){
+//     return NextResponse.json({
+//         messaage:'Unauthorized User',
+//         success:false,
+//     })
+// }
 // console.log(authHeader)
     const token = (await cookies()).get("authtoken")?.value;
     if (!token) {
@@ -33,9 +33,9 @@ if(!authHeader || id !== idHeaderofUser){
     }
 
     // Prevent users from fetching others' data
-    if (decoded?.id !== id) {
-      return NextResponse.json({ message: "Unauthorized user" }, { status: 403 });
-    }
+    // if (decoded?.id !== id) {
+    //   return NextResponse.json({ message: "Unauthorized user" }, { status: 403 });
+    // }
 
     const userById = await userModel
       .findById(id)
